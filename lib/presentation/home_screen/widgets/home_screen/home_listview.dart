2c96cd58fee998db43931a3c78142db1fb0461b3
Nihalmullungal <nihalmullungal@gmail.com>
2024-04-07 08:55:07 +0530

    after finish

--- lib/presentation/home_screen/widgets/home_screen/home_listview.dart
+++ lib/presentation/home_screen/widgets/home_screen/home_listview.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -30,9 +32,9 @@ class HomeListView extends StatelessWidget {
           return Center(
               child: Lottie.asset("assets/lottie/No data.json", height: 250));
         } else {
-          homec.add(ShowUserEvent(val: snapshot.data!.docs));
           return BlocBuilder<HomeScreenBloc, HomeScreenState>(
             builder: (context, state) {
+              homec.add(ShowUserEvent(val: snapshot.data!.docs));
               return homec.userList.isNotEmpty
                   ? Expanded(
                       child: SizedBox(
