7912cdfc3c2a96191c4c9e9462f317260fbebf9c
Nihalmullungal <nihalmullungal@gmail.com>
2024-04-07 08:13:54 +0530

    after finish

--- lib/presentation/home_screen/widgets/modal_sheet/custom_app_bar.dart
+++ lib/presentation/home_screen/widgets/modal_sheet/custom_app_bar.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:totalx/application/homescreen_bloc/homescreen_bloc.dart';
-import 'package:totalx/application/homescreen_bloc/homescreen_event.dart';
 import 'package:totalx/application/homescreen_bloc/homescreen_state.dart';
-import 'package:totalx/presentation/common/custom_snackbar.dart';
 import 'package:totalx/presentation/common/font_theme.dart';
 import 'package:totalx/presentation/home_screen/widgets/custom_dialog.dart';
 import 'package:totalx/presentation/login_screen/login_screen.dart';
